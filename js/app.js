f8c0f8180f5a166ed9b79ad16f3abf655a8d8145
AJ Longstreet <ajlongstreet@AJs-MacBook-Pro.local>
2018-06-13 16:24:42 -0400

    adds pagination

--- js/app.js
+++ js/app.js
@@ -1,24 +1,64 @@
 const $students = $(".student-item");
-$students.remove();
+const $studentContainer = $(".student-list");
 
 const selectStudents = (studentList, pageIndex) => {
-    return studentList.splice(pageIndex * 10 - 10, pageIndex * 10);
-}
+  return studentList.slice(pageIndex * 10 - 10, pageIndex * 10);
+};
 
-const buttonCount = (studentList) => {
-    return Math.ceil(studentList.length / 10);
-}
+const showStudents = (studentList, pageNumber) => {
+  $studentContainer.empty();
+  $studentContainer.append(selectStudents(studentList, pageNumber));
+};
 
-const createButtons = (buttonCount) => {
-    const arrayOfButtons = ["<ul class=pagination>"];
-    let index = 1;
-    while (arrayOfButtons.length < buttonCount) {
-        arrayOfButtons.push(`<li><a>${index}</a></li>`)
-        index++;
-    }
-    arrayOfButtons.push("</ul>")
-    return arrayOfButtons.join("");
-}
+const buttonCount = studentList => {
+  return Math.ceil(studentList.length / 10);
+};
 
-$(".student-list").append(selectStudents($students, 1));
-$(".student-list").append(createButtons(buttonCount($students)));
+const createButtons = buttonCount => {
+  const arrayOfButtons = ["<ul class=pagination>"];
+  let index = 1;
+  while (arrayOfButtons.length <= buttonCount) {
+    arrayOfButtons.push(`<li><a>${index}</a></li>`);
+    index++;
+  }
+  arrayOfButtons.push("</ul>");
+  return arrayOfButtons.join("");
+};
+
+const createListeners = $element => {
+  $element.click(event => {
+    showStudents($students, event.target.innerText);
+  });
+};
+
+// const createSearch = () => {
+//   $(".page-header").append(`<input class=search js_search>`);
+//   $(".search").keyup((event) => onSearch(event.target.value))};
+
+// const searchFilter = (studentList, searchInput) => {
+//   return studentList.filter(() => {
+//     if (this.find(searchInput)) {
+//       return true;
+//     }
+//   });
+// };
+
+// const onSearch = (searchTerm) => {
+//     const $searchlist = searchFilter($students, searchTerm)
+//     $(".pagination").remove()
+//     $studentContainer.after(createButtons(buttonCount($searchlist)));
+//     showStudents($students, 1);
+// }
+
+const onPageLoad = () => {
+  $studentContainer.after(createButtons(buttonCount($students)));
+  showStudents($students, 1);
+  createListeners($(".pagination").children());
+  createSearch();
+
+  console.log(
+    `${buttonCount($students)} buttons created for ${$students.length} students`
+  );
+};
+
+onPageLoad();
